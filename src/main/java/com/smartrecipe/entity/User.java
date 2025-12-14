be0dde5cf1a35a12bfa35ef005dc3ddefcde97b4
Astrocatt <josemiguel0328@gmail.com>
2025-12-14 00:50:37 -0500

    feat: User entity added

---
+++ src/main/java/com/smartrecipe/entity/User.java
@@ -0,0 +1,37 @@
+package com.smartrecipe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, updatable = false, name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+
+}
