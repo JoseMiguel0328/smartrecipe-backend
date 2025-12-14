2b63726978ac7d50a5ce1bd8cd4771555092b5f5
Astrocatt <josemiguel0328@gmail.com>
2025-12-14 00:02:31 -0500

    chore: Spring Boot project

---
+++ src/test/java/com/smartrecipe/SmartRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
