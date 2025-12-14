f8699bd5afbe65d6a96f0f75f797570209163c6c
Astrocatt <josemiguel0328@gmail.com>
2025-12-14 00:55:23 -0500

    feat: Recipe entity added

---
+++ src/main/java/com/smartrecipe/entity/Recipe.java
@@ -0,0 +1,43 @@
+package com.smartrecipe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recipes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(name = "preparation_time")
+    private Integer preparationTime;
+
+    @Column(nullable = false)
+    private Boolean isPublic;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
