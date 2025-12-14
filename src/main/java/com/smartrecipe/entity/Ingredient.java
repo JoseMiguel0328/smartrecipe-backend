e2b3455fc6edcffc88b2d5ea91acbf4245abb9bc
Astrocatt <josemiguel0328@gmail.com>
2025-12-14 00:57:41 -0500

    feat: Ingredient entity added

---
+++ src/main/java/com/smartrecipe/entity/Ingredient.java
@@ -0,0 +1,23 @@
+package com.smartrecipe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "ingredients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
